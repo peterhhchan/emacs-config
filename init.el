5ff3b4e1f4acf00dce8da4828d9ef0cfe9bc3153
peterhhchan <peterhhchan@gmail.com>
2016-10-09 02:06:23 -0400

    Initial setup fix

--- init.el
+++ init.el
@@ -50,6 +50,9 @@
     ;; https://github.com/clojure-emacs/cider
     cider
 
+    ;;
+    helm
+
     ;; allow ido usage in as many contexts as possible. see
     ;; customizations/navigation.el line 23 for a description
     ;; of ido

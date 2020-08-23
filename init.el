5f28b2191bc1377a64bf2490479f5a3e2daedad9
peterhhchan <peterhhchan@gmail.com>
2020-08-23 04:46:05 -0400

    Update python files.

--- init.el
+++ init.el
@@ -15,32 +15,7 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-;; Define package repositories
-;;(require 'package)
 
-;; (setq package-archives
-;;       '(("MELPA Stable" . "https://stable.melpa.org/packages/")
-;;         ("MELPA"        . "https://melpa.org/packages/")
-;;         ("tromey"       . "http://tromey.com/elpa"))
-;;       package-archive-priorities
-;;       '(("MELPA Stable" . 10)
-;;         ("MELPA"        . 0)
-;;         ("tromey"       . 0)))
-
-;; Load and activate emacs packages. Do this first so that the
-;; packages are loaded before you start trying to modify them.
-;; This also sets the load path.
-;;(package-initialize)
-
-;; Download the ELPA archive description if needed.
-;; This informs Emacs about the latest versions of all packages, and
-;; makes them available for download.
-;; (when (not package-archive-contents)
-;;   (package-refresh-contents))
-
-;; The packages you want installed. You can also install these
-;; manually with M-x package-install
-;; Add in your own as you wish:
 (defvar my-packages
   '(;; makes handling lisp expressions much, much easier
     ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
@@ -77,6 +52,7 @@
 
     ;; project navigation
     projectile
+
 
     ;; colorful parenthesis matching
     rainbow-delimiters

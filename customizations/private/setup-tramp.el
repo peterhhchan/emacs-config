5d3f8c91c2987ecb5fad937941a0de8d0c7ddea9
peterhhchan <peterhhchan@gmail.com>
2020-10-27 18:28:09 -0400

    Add tramp setup file.  (Intentionally included even though its gitignored.)

---
+++ customizations/private/setup-tramp.el
@@ -0,0 +1,15 @@
+;; tramp config
+(setq tramp-default-method "ssh")
+
+(put 'erase-buffer 'disabled nil)
+
+(put 'downcase-region 'disabled nil)
+
+
+(set-variable 'nrepl-use-ssh-fallback-for-remote-hosts "true")
+
+(add-to-list 'tramp-remote-process-environment "CHROME_DRIVER_PATH=/home/ec2-user/resources/drivers/chromedriver")
+
+(add-to-list 'tramp-remote-process-environment "CHROME_BIN_PATH=/usr/bin/google-chrome")
+
+(add-to-list 'tramp-remote-process-environment "DISPLAY=:99")
